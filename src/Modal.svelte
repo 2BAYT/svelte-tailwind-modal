a67da43a10ce7b0b792237d0aa06d84c20153ee8
gurayakyuz <gur.akyuz@gmail.com>
2021-02-04 15:32:10 +0300

    v0.1.4

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -7,6 +7,7 @@
 
     export let outSideClick = null;
     export let autoBottomForMobile = false;
+    export let safePaddingBottom = false;
 
     export function show(){
         visible = true;
@@ -28,15 +29,14 @@
 
 {#if visible}
 <div class="z-40 fixed {autoBottomForMobile ? 'bottom-0' : 'inset-0'} sm:inset-0  flex items-center justify-center">
-        
-        <div class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        <div use:clickOutside use:escapeKeyPress  on:escapeKeyPress={closeClicked} on:clickOutside={closeClicked} class="{full ? 'flex h-full w-full' :''} transform transition-all " role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+    <div class="fixed inset-0 transition-opacity">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    </div>
+    <div use:clickOutside use:escapeKeyPress  on:escapeKeyPress={closeClicked} on:clickOutside={closeClicked} class="{autoBottomForMobile ? 'bg-white sm:rounded-md' :''} {safePaddingBottom ?  'safe-padding-bottom':''} {full ? 'flex h-full w-full' :''} transform transition-all " role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <div class="{full ? 'flex h-full w-full' :''}">
             <slot></slot>
         </div>
     </div>
-
+</div>
 {/if}
 
